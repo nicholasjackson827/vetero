c4834676ec3f58fdb81707e78db70e772820b232
Nicholas Jackson <nicholasjackson827@gmail.com>
2020-02-28 12:03:12 -0500

    Updated the seed data

--- vetero-server/src/main/resources/data.sql
+++ vetero-server/src/main/resources/data.sql
@@ -3,9 +3,9 @@ delete from users_locations;
 delete from users;
 delete from locations;
 
-insert into users (id, username) values (9980, "Bob");
-insert into users (id, username) values (9981, "Tom");
-insert into users (id, username) values (9982, "Joe");
+insert into users (id, username) values (9980, "Nicholas");
+insert into users (id, username) values (9981, "Brian");
+insert into users (id, username) values (9982, "Keith");
 insert into users (id, username) values (9983, "Suzy");
 insert into users (id, username) values (9984, "Sally");
 
@@ -21,7 +21,16 @@ insert into users_locations (user_id, locations_id) values (9982, 9982);
 insert into users_locations (user_id, locations_id) values (9983, 9983);
 insert into users_locations (user_id, locations_id) values (9984, 9984);
 
--- Give Bob a few extra locations
-insert into users_locations (user_id, locations_id) values (9980, 9981);
+-- Give some users a few extra locations
 insert into users_locations (user_id, locations_id) values (9980, 9982);
 insert into users_locations (user_id, locations_id) values (9980, 9983);
+insert into users_locations (user_id, locations_id) values (9980, 9984);
+
+insert into users_locations (user_id, locations_id) values (9981, 9980);
+insert into users_locations (user_id, locations_id) values (9981, 9982);
+insert into users_locations (user_id, locations_id) values (9981, 9983);
+insert into users_locations (user_id, locations_id) values (9981, 9984);
+
+insert into users_locations (user_id, locations_id) values (9982, 9980);
+insert into users_locations (user_id, locations_id) values (9982, 9983);
+insert into users_locations (user_id, locations_id) values (9982, 9984);
